bfe6ce8df68886ad5edf9cb5334845394e97aeeb
Purplepeak <shrpeak@gmail.com>
2014-02-10 10:03:16 -0800

    Update file_info.php

--- templates/file_info.php
+++ templates/file_info.php
@@ -1,7 +1,7 @@
 <?php include 'header.php';?>
     <div class="download">
       <h1><a href = "<?= BASE_URL ."/". spChars($fileData->link) ?>" ><?= spChars($fileData->name) ?> <small>(<?= spChars(Helper::formatBytes($fileData->size)) ?>)</small></a></h1>
-    <?php if ($fileData->isHasThumbnail()): ?>
+    <?php if ($fileData->hasThumbnail()): ?>
       <a href = "<?= BASE_URL ."/". spChars($fileData->link) ?>"><img src="<?= spChars($thumbnail) ?>"></a>
     <?php endif; ?>
       <a href = "<?= BASE_URL ."/". spChars($fileData->link) ?>" >
