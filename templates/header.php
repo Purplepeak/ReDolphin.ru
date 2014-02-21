8e43b0b9750a94246062af5663b0d9fea27fc7c4
Purplepeak <shrpeak@gmail.com>
2014-02-21 22:45:34 +0400

    navbar style
    navbar style

--- templates/header.php
+++ templates/header.php
@@ -18,22 +18,19 @@
     
   </head>
   <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="<?= BASE_URL ?>/">ReDolphin</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="rd-tab"><a href="<?= BASE_URL ?>/">Главная</a></li>
-            <li class="rd-tab"><a href="">Файлы</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div class="navbar navbar-default navbar-fixed-top">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target=".navbar-responsive-collapse">
+				<span class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?= BASE_URL ?>/">ReDolphin</a>
+		</div>
+		<div class="navbar-collapse collapse navbar-responsive-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="<?= BASE_URL ?>/">Главная</a></li>
+				<li><a href="#">Файлы</a></li>
+			</ul>
+		</div>
+	</div>
