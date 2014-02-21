15bb46da59759e3aaafe9ee20475ae506103d135
Purplepeak <shrpeak@gmail.com>
2014-02-21 05:08:13 +0400

    button madness
    button madness

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,6 +1,3 @@
 <?php include 'header.php';?>
 <?php include 'upload_form.php';?>
-<?php if ($flash['error']): ?>
-      <div class="alert alert-danger"><?= spChars($flash['error']) ?></div>
-      <?php endif; ?>
 <?php include 'footer.php';?>
--- templates/upload_form.php
+++ templates/upload_form.php
@@ -1,7 +1,11 @@
 <div class="table">
   <div class="cell">
     <div class="rd-upload">
-      <div id='alert-message' class="rd-alert"></div>
+      <div id='alert-message' class="rd-alert">
+        <?php if ($flash['error']): ?>
+            <div class="alert alert-danger"><?= spChars($flash['error']) ?></div>
+        <?php endif; ?>
+      </div>
       <form name = "upload_form" class="upload_form"  enctype="multipart/form-data" action="<?= BASE_URL ?>/upload"  method="POST" onsubmit="return showFileSize(<?= intval($maxFileSize)?>, this)" >
           <input type="hidden" name="MAX_FILE_SIZE" value="<?= $maxFileSize * pow(10, 6) ?>" >
           <div class="file-wrapper">
